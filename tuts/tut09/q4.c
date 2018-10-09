4cb2f5fdd67df7191bc892d113100fe18a2880e5
nick-hiebl <n.hiebl@student.unsw.edu.au>
2018-10-09 16:34:03 +1100

    Working files for tut 9

--- tuts/tut09/q4.c
+++ tuts/tut09/q4.c
@@ -12,23 +12,32 @@ void delay(int);
 int main(void) {
     int pid;
 
+    sem_t *print_lock;
+
+    print_lock = sem_open("print_lock", O_CREAT, 0644, 1);
+
     setbuf(stdout, NULL);
     if ((pid = fork()) != 0) {
         for (int i = 0; i < 5; i++) {
+            sem_wait(print_lock);
             for (int j = 0; j < 26; j++)
                 putchar('a'+j);
             putchar('\n');
+            sem_post(print_lock);
             delay(100000);
         }
     }
     else {
         for (int i = 0; i < 5; i++) {
+            sem_wait(print_lock);
             for (int j = 0; j < 26; j++)
                 putchar('A'+j);
             putchar('\n');
+            sem_post(print_lock);
             delay(100000);
         }
     }
+    sem_destroy(print_lock);
     return 0;
 }
 
--- tuts/tut09/q6.c
+++ tuts/tut09/q6.c
@@ -1,4 +1,7 @@
-// gcc q6.c -lcurl -o q6
+// Fetch data from a URL, separating header and body
+// Adapted from an example on https://curl.haxx.se/
+// Written by Daniel Sternberg
+// Modified by John Shepherd
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -7,36 +10,43 @@
 
 static size_t write_data(void *ptr, size_t size, size_t nmemb, void *stream)
 {
-    size_t written = fwrite(ptr, size, nmemb, (FILE *)stream);
-    return written;
+   size_t written = fwrite(ptr, size, nmemb, (FILE *)stream);
+   return written;
 }
 
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
-    CURL *cp;
+   CURL *cp;
 
-    // check that they gave us a URL
-    if (argc < 2) {
-        fprintf(stderr, "Usage %s URL\n", argv[0]);
-        exit(1);
-    }
-    // init the curl session
-    curl_global_init(CURL_GLOBAL_ALL);
-    cp = curl_easy_init();
-    // set URL to get
-    curl_easy_setopt(cp, CURLOPT_URL, argv[1]);
-    // no progress meter please
-    curl_easy_setopt(cp, CURLOPT_NOPROGRESS, 1L);
-    // send all data to this function
-    curl_easy_setopt(cp, CURLOPT_WRITEFUNCTION, write_data);
-    // we want the headers be written to stdout
-    curl_easy_setopt(cp, CURLOPT_HEADERDATA, headf);
-    // we want the body be written to stdout
-    curl_easy_setopt(cp, CURLOPT_WRITEDATA, bodyf);
-    // get it!
-    curl_easy_perform(cp);
-    // cleanup curl stuff
-    curl_easy_cleanup(cp);
+   // check that they gave us a URL
+   if (argc < 2) {
+      fprintf(stderr, "Usage %s URL\n", argv[0]);
+      exit(1);
+   }
+   // init the curl session
+   curl_global_init(CURL_GLOBAL_ALL);
+   cp = curl_easy_init();
+   // set URL to get
+   curl_easy_setopt(cp, CURLOPT_URL, argv[1]);
+   // no progress meter please
+   curl_easy_setopt(cp, CURLOPT_NOPROGRESS, 1L);
+   // send all data to this function
+   curl_easy_setopt(cp, CURLOPT_WRITEFUNCTION, write_data);
 
-    return 0;
+   FILE *headf = fopen("head.txt", "w");
+   FILE *bodyf = fopen("body.txt", "w");
+
+   // we want the headers be written to stdout
+   curl_easy_setopt(cp, CURLOPT_HEADERDATA, headf);
+   // we want the body be written to stdout
+   curl_easy_setopt(cp, CURLOPT_WRITEDATA, bodyf);
+   // get it!
+   curl_easy_perform(cp);
+   // cleanup curl stuff
+   curl_easy_cleanup(cp);
+
+   fclose(headf);
+   fclose(bodyf);
+
+   return 0;
 }
